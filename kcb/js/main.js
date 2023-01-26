c4bd9ec94a2731405969bd45d62448446639ee3d
Vimal Kaul <kaulvimal@gmail.com>
2023-01-26 19:21:42 +0530

    removed kcb file

--- kcb/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready((function() {
-    $(".menu_toggle").click((function() {
-        $("body").toggleClass("nav-open")
-    }));
-    $(".menu_overlay, .book_cta").click((function() {
-        $("body").removeClass("nav-open")
-    }));
-}));
-
